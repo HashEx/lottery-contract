23136c71c90e64e00044c5e1ec19e419273b18ce
Gleb Zykov <g.zykov@gtc-spb.ru>
2017-12-20 13:50:56 +0300

    modifier lottery runners, setting next ticket price, pausable contract

--- contracts/DeLottery.sol
+++ contracts/DeLottery.sol
@@ -1,43 +1,66 @@
 pragma solidity ^0.4.18;
 import 'zeppelin-solidity/contracts/math/SafeMath.sol';
-import 'zeppelin-solidity/contracts/ownership/Ownable.sol';
+import 'zeppelin-solidity/contracts/lifecycle/Pausable.sol';
 
 /*
 	подумать - что есть во время розыгрыша меняется стоимость
 */
 
-contract DeLottery is Ownable {
+contract DeLottery is Pausable {
 	using SafeMath for uint256;
 
 	uint32 public constant QUORUM = 3;
 
 	address[] participants;
+	uint participantsCount;
 
 	uint public ticketPrice = 1 ether;
 
 	uint public prizeFund = 0;
 
+	uint public nextTicketPrice = 0;
+
 	mapping(address => mapping(address => uint)) prizes;
 
+	mapping(address => bool) lotteryRunners;
+
+	/**
+   	* @dev Throws if called by any account other than the owner.
+   	*/
+   	modifier canRunLottery() {
+   		require(lotteryRunners[msg.sender]);
+   		_;
+   	}
+
 	function DeLottery() public {
+		setAsLotteryRunner(msg.sender, true);
 	}
 
-	function buyTicket() public payable {
+	function buyTicket() public payable whenNotPaused {
 		require(msg.value == ticketPrice);
 		require(!isContract(msg.sender));
 		participants.push(msg.sender);
+		participantsCount++;
 		prizeFund = prizeFund.add(ticketPrice);
 	}
 
-	function setTicketPrice(uint _tickerPrice) external onlyOwner {
-		ticketPrice = _tickerPrice;
+	function setTicketPrice(uint tickerPrice) external onlyOwner {
+		nextTicketPrice = tickerPrice;
 	}
 
-	function runLottery() external onlyOwner {
-		uint participantsCount = participants.length;
+	function setAsLotteryRunner(address addr, bool canRunLottery) public onlyOwner {
+		lotteryRunners[addr] = canRunLottery;
+	}
+
+	function runLottery() external whenNotPaused canRunLottery {
 		require(participantsCount >= QUORUM);
 
-		uint winnersCount = participantsCount / 10 + 1;
+		uint winnersCount;
+		if(participantsCount < 10) {
+			winnersCount = 1;
+		} else {
+			winnersCount = participantsCount / 10;
+		}
 		uint winnerPrize = prizeFund / winnersCount * 19 / 20;
 
 		int[] memory winners = new int[](winnersCount);
@@ -54,14 +77,21 @@ contract DeLottery is Ownable {
 			winners[i] = int(lastWinner);
 		}
 
+		//set initial state
 		prizeFund = 0;
+		participantsCount = 0;
 
 		for(uint k = 0; k < winnersCount; k++) {
 			participants[uint(winners[k])].transfer(winnerPrize);
 		}
+
+		if(nextTicketPrice > 0) {
+			ticketPrice = nextTicketPrice;
+			nextTicketPrice = 0;
+		}
 	}
 
-	function generateNextWinner(uint previousWinner, int[] winners, uint participantsCount) returns(uint) {
+	function generateNextWinner(uint previousWinner, int[] winners, uint participantsCount) private returns(uint) {
 		uint nonce = 0;
 		uint winner = generateWinner(previousWinner, nonce, participantsCount);
 
@@ -73,7 +103,7 @@ contract DeLottery is Ownable {
 		return winner;
 	}
 
-	function generateWinner(uint previousWinner, uint nonce, uint participantsCount) internal returns (uint winner) {
+	function generateWinner(uint previousWinner, uint nonce, uint participantsCount) private returns (uint winner) {
 		return uint(keccak256(uint(block.blockhash(block.number - 1)), previousWinner, nonce)) % participantsCount;
 	}
 
